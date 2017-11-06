dcb556a11332b61a102f8606342d812bb70b45f2
alexgriff <alexalexgriffith@gmail.com>
2017-11-06 10:08:35 -0500

    pre ajax lecture refactored starter code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const fetchBooks = () => data.books;
+
+// Google Books API docs:
+// https://developers.google.com/books/docs/v1/using
+// search term:
+// `https://www.googleapis.com/books/v1/volumes?q=ruby+programming`
+// author name:
+// `https://www.googleapis.com/books/v1/volumes?q=inauthor:eco`
+
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('loaded');
+  const app = new App();
+  app.addAllEventListeners();
+
+  app.renderBooks(fetchBooks().map(bookData => new Book(bookData)));
 });
